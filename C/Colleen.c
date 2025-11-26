88747b78f0dad167aa9f50dcb5312320bf1b94b9
zigeuh <zigeuh13@gmail.com>
2025-11-26 14:14:50 +0100

    Added 2 two comments, one inside of main function, one outside of program | Added another function and called it

--- C/Colleen.c
+++ C/Colleen.c
@@ -1,2 +1,4 @@
 #include <stdio.h>
-int main() {char *str="#include <stdio.h>%cint main() {char *str=%c%s%c; printf(str, 10, 34, str, 34);}"; printf(str, 10, 34, str, 34);}
+/*This is a comment outside of the program*/
+int if_one() {int c = 1; if(c == 1) return 1; return 0;}
+int main() {/*this is a comment*/char *str="#include <stdio.h>%c/*This is a comment outside of the program*/%cint if_one() {int c = 1; if(c == 1) return 1; return 0;}%cint main() {/*this is a comment*/char *str=%c%s%c; if_one(); printf(str, 10, 10, 10, 34, str, 34);}"; if_one(); printf(str, 10, 10, 10, 34, str, 34);}
