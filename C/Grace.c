50dabba32d738364dc9c8d7cd92983371e74a241
zigeuh <zigeuh13@gmail.com>
2025-11-26 15:02:32 +0100

    Grace now has 3 macros

--- C/Grace.c
+++ C/Grace.c
@@ -2,10 +2,10 @@
 #include <fcntl.h>
 
 #define START int main() {
+#define STR ("#include <stdio.h>%c#include <fcntl.h>%c%c#define START int main() {%c#define STR (%c%s%c)%c#define END return 0; }%c%cSTART%c%cint fd = open(%cGrace_kid.c%c, O_CREAT | O_TRUNC | O_WRONLY, 0644);%c%cdprintf(fd, STR, 10, 10, 10, 10, 34, STR, 34, 10, 10, 10, 10, 9, 34, 34, 10, 9, 10);%cEND")
 #define END return 0; }
 
 START
-	char *str ="#include <stdio.h>%c#include <fcntl.h>%c%c#define START int main() {%c#define END return 0; }%c%cSTART%c%cchar *str =%c%s%c;%c%cint fd = open(%cGrace_kid.c%c, O_CREAT | O_TRUNC | O_WRONLY, 0644);%c%cdprintf(fd, str, 10, 10, 10, 10, 10, 10, 10, 9, 34, str, 34, 10, 9, 34, 34, 10, 9, 10);%cEND";
 	int fd = open("Grace_kid.c", O_CREAT | O_TRUNC | O_WRONLY, 0644);
-	dprintf(fd, str, 10, 10, 10, 10, 10, 10, 10, 9, 34, str, 34, 10, 9, 34, 34, 10, 9, 10);
+	dprintf(fd, STR, 10, 10, 10, 10, 34, STR, 34, 10, 10, 10, 10, 9, 34, 34, 10, 9, 10);
 END
