2952f3d74cb5fefb365d3e6fbebe16618e20c839
zigeuh <zigeuh13@gmail.com>
2026-01-31 15:40:40 +0100

    Save

--- C/Sully.c
+++ C/Sully.c
@@ -4,11 +4,10 @@
 
 int main() {
 	int count = 5;
-	char *str = "#include <stdio.h>%c#include <stdlib.h>%c#include <fcntl.h>%c%cint main() {%c%cint count = %d;%c%cchar *str = %c%s%c;%c%cchar name[9] = %cSully5.c%c; count--; name[5] = count + 48;%c%cint fd = open(name, O_CREAT | O_WRONLY, 0644);%c%cdprintf(fd, str, 10, 10, 10, 10, 10, 9, count, 10, 9, 34, str, 34, 10, 9, 34, 34, 10, 9, 10, 9, 10);%c}";
+	char *str = "#include <stdio.h>%c#include <stdlib.h>%c#include <fcntl.h>%c%cint main() {%c%cint count = %d;%c%cchar *str = %c%s%c;%c%cchar name[9] = %cSully5.c%c; count--; name[5] = count + 48;%c%cint fd = open(name, O_CREAT | O_WRONLY, 0644); name[6] = 0;%c%cchar *test = %c%c%c%c; dprintf(fd, str, 10, 10, 10, 10, 10, 9, count, 10, 9, 34, str, 34, 10, 9, 34, 34, 10, 9, 10, 9, 34, 37, 115, 34,10, 9, 34, test, test, test, 34, 10, 9, 10);%c%cchar command[256]; snprintf(command, sizeof(command), %cclang -Wall -Wextra -Werror -o %s %s.c ; ./%s%c, name, name, name);%c%cint ret = system(command); return (ret);%c}";
 	char name[9] = "Sully5.c"; count--; name[5] = count + 48;
-	int fd = open(name, O_CREAT | O_WRONLY, 0644);
-	name[6] = 0;
-	dprintf(fd, str, 10, 10, 10, 10, 10, 9, count, 10, 9, 34, str, 34, 10, 9, 34, 34, 10, 9, 10, 9, 10);
+	int fd = open(name, O_CREAT | O_WRONLY, 0644); name[6] = 0;
+	char *test = "%s"; dprintf(fd, str, 10, 10, 10, 10, 10, 9, count, 10, 9, 34, str, 34, 10, 9, 34, 34, 10, 9, 10, 9, 34, 37, 115, 34,10, 9, 34, test, test, test, 34, 10, 9, 10);
 	char command[256]; snprintf(command, sizeof(command), "clang -Wall -Wextra -Werror -o %s %s.c ; ./%s", name, name, name);
 	int ret = system(command); return (ret);
 }
