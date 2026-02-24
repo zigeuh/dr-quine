520b9277c5383b1cfbedc37d5f06578bb3efbdcb
zigeuh <zigeuh13@gmail.com>
2026-02-24 16:09:57 +0100

    Improved Grace with subject's define example

--- C/Grace/Grace.c
+++ C/Grace/Grace.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
 #include <fcntl.h>
 
-#define START int main() {
-#define STR ("#include <stdio.h>%c#include <fcntl.h>%c%c#define START int main() {%c#define STR (%c%s%c)%c#define END return 0; }%c%cSTART%c%cint fd = open(%cGrace_kid.c%c, O_CREAT | O_TRUNC | O_WRONLY, 0644);%c%cdprintf(fd, STR, 10, 10, 10, 10, 34, STR, 34, 10, 10, 10, 10, 9, 34, 34, 10, 9, 10);%cEND")
-#define END return 0; }
+#define FILENAME ("Grace_kid.c")
+#define FT() int main(){int fd = open(FILENAME, O_CREAT | O_TRUNC | O_WRONLY, 0644);dprintf(fd, STR, STR, 10, 34);}
+#define STR ("#include <stdio.h>%2$c#include <fcntl.h>%2$c%2$c#define FILENAME (%3$cGrace_kid.c%3$c)%2$c#define FT() int main(){int fd = open(FILENAME, O_CREAT | O_TRUNC | O_WRONLY, 0644);dprintf(fd, STR, STR, 10, 34);}%2$c#define STR (%3$c%1$s%3$c)%2$c%2$cFT()")
 
-START
-	int fd = open("Grace_kid.c", O_CREAT | O_TRUNC | O_WRONLY, 0644);
-	dprintf(fd, STR, 10, 10, 10, 10, 34, STR, 34, 10, 10, 10, 10, 9, 34, 34, 10, 9, 10);
-END
+FT()
