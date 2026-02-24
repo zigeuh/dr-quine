c7c923e3ac7300c4bdde426d2d6eda1b01c1d8fc
zigeuh <zigeuh13@gmail.com>
2026-02-24 18:42:11 +0100

    Finished python Grace + Added comments on Colleen (as asked by the subject) + Added another function on Colleen (as asked by the subject)

--- Python/Colleen/Colleen.py
+++ Python/Colleen/Colleen.py
@@ -1,5 +1,13 @@
+# This is a comment outside the main function
+def useless():
+    if 0 == 0:
+        return 1
+    else:
+        return 0
+
 def main():
-    str = 'def main():\n    str = %r\n    print(str %% str)\n\nif __name__ == "__main__":\n    main()'
+    # This is a comment inside the main function
+    str = '# This is a comment outside the main function\ndef useless():\n    if 0 == 0:\n        return 1\n    else:\n        return 0\n\ndef main():\n    # This is a comment inside the main function\n    str = %r\n    print(str %% str)\n\nif __name__ == "__main__":\n    main()'
     print(str % str)
 
 if __name__ == "__main__":
---
+++ Python/Grace/Grace.py
@@ -0,0 +1,9 @@
+FILENAME = "Grace_kid.py"
+STR = 'FILENAME = "Grace_kid.py"\nSTR = %r\n\ndef main():\n    with open(FILENAME, "w") as file:\n        file.write(STR %% STR)\n\nif __name__ == "__main__":\n    main()\n'
+
+def main():
+    with open(FILENAME, "w") as file:
+        file.write(STR % STR)
+
+if __name__ == "__main__":
+    main()
