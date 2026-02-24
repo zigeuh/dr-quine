6185be00fb91734d82f3204e9e9c6c3aa4c67fca
zigeuh <zigeuh13@gmail.com>
2026-02-24 15:09:03 +0100

    Made Colleen more readable + Now using positional parameters + Fixed Makefile for colleen_diff rule

--- C/Colleen/Colleen.c
+++ C/Colleen/Colleen.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
 /*This is a comment outside of the program*/
 int if_one() {int c = 1; if(c == 1) return 1; return 0;}
-int main() {/*this is a comment*/char *str="#include <stdio.h>%c/*This is a comment outside of the program*/%cint if_one() {int c = 1; if(c == 1) return 1; return 0;}%cint main() {/*this is a comment*/char *str=%c%s%c; if_one(); printf(str, 10, 10, 10, 34, str, 34);}"; if_one(); printf(str, 10, 10, 10, 34, str, 34);}
+int main() {
+	/*this is a comment*/
+	char *str="#include <stdio.h>%2$c/*This is a comment outside of the program*/%2$cint if_one() {int c = 1; if(c == 1) return 1; return 0;}%2$cint main() {%2$c%3$c/*this is a comment*/%2$c%3$cchar *str=%4$c%s%4$c;%2$c%3$cif_one();%2$c%3$cprintf(str, str, 10, 9, 34);%2$c}";
+	if_one();
+	printf(str, str, 10, 9, 34);
+}
