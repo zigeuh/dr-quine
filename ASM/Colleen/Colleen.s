c67121710debc22ac89083407a4f0421562fed8d
zigeuh <zigeuh13@gmail.com>
2026-02-25 17:40:06 +0100

    Added additional function in ASM/python quines

--- ASM/Colleen/Colleen.s
+++ ASM/Colleen/Colleen.s
@@ -1,9 +1,13 @@
 ; This is a comment outside
-fmt: db "; This is a comment outside%2$cfmt: db %3$c%s%3$c, 0%2$csection .text%2$c    global main%2$c    extern printf%2$cmain:%2$c    lea rdi, [rel fmt] ; This is a comment in the entry point%2$c    lea rsi, [rel fmt]%2$c    mov rdx, 10%2$c    mov rcx, 34%2$c    xor eax, eax%2$c    sub rsp, 8%2$c    call printf wrt ..plt%2$c    add rsp, 8%2$c    mov eax, 0%2$c    ret", 0
+fmt: db "; This is a comment outside%2$cfmt: db %3$c%s%3$c, 0%2$csection .text%2$c    global main%2$c    extern printf%2$cother:%2$c    mov eax, 1%2$c    ret%2$cmain:%2$c    call other%2$c    lea rdi, [rel fmt] ; This is a comment in the entry point%2$c    lea rsi, [rel fmt]%2$c    mov rdx, 10%2$c    mov rcx, 34%2$c    xor eax, eax%2$c    sub rsp, 8%2$c    call printf wrt ..plt%2$c    add rsp, 8%2$c    mov eax, 0%2$c    ret", 0
 section .text
     global main
     extern printf
+other:
+    mov eax, 1
+    ret
 main:
+    call other
     lea rdi, [rel fmt] ; This is a comment in the entry point
     lea rsi, [rel fmt]
     mov rdx, 10
--- Python/Colleen/Colleen.py
+++ Python/Colleen/Colleen.py
@@ -11,7 +11,8 @@ def main():
     '''
     This is a comment inside the main function
     '''
-    str = '\'\'\'\nThis is a comment outside the main function\n\'\'\'\ndef useless():\n    if 0 == 0:\n        return 1\n    else:\n        return 0\n\ndef main():\n    \'\'\'\n    This is a comment inside the main function\n    \'\'\'\n    str = %r\n    print(str %% str)\n\nif __name__ == "__main__":\n    main()'
+    useless()
+    str = '\'\'\'\nThis is a comment outside the main function\n\'\'\'\ndef useless():\n    if 0 == 0:\n        return 1\n    else:\n        return 0\n\ndef main():\n    \'\'\'\n    This is a comment inside the main function\n    \'\'\'\n    useless()\n    str = %r\n    print(str %% str)\n\nif __name__ == "__main__":\n    main()'
     print(str % str)
 
 if __name__ == "__main__":
